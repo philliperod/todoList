0f73813ca86c63122a8b01c54c59e87024428f94
Phillipe Rodriguez <philliperod@gmail.com>
2020-10-10 11:57:06 -0500

    added code for testing GET route

--- app.js
+++ app.js
@@ -6,7 +6,13 @@ const app = express();
 // reminder that this is a GET route that sends the code to the browser
 // when the user is trying to gain access to the home route
 app.get('/', (req, res) => {
-  res.send('Hello');
+  let today = new Date();
+
+  if (today.getDay() === 6 || today.getDay() === 0) {
+    res.send("Oh yeah. It's the weekend!");
+  } else {
+    res.send("Ugh. Every day feels like Monday until it's Friday.");
+  }
 });
 
 // listening to port 3000
